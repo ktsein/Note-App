0bdaa82f6fd102038f6bbb9e2c536416c3a94fdd
Kyaw Tun Sein <kyawtunsein.gla@gmail.com>
2016-11-07 16:40:51 +0000

    Add files(models, spec, html)

---
+++ js/note-model.js
@@ -0,0 +1,11 @@
+(function(exports){
+  function takeNote(note){
+    var text = String(note);
+  };
+  function returnNote(note){
+    return note;
+  };
+
+  exports.takeNote = takeNote;
+  exports.returnNote = returnNote;
+})(this);
---
+++ test/note_model_spec.js
@@ -0,0 +1,6 @@
+// var takeNote = require("../js/note-model").takeNote;
+// var returnNote = require("../js/note-model").returnNote;
+
+function testTakeNote(){
+  var note_1 = new
+}
